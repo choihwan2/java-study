7663779ee69c45788919b3489f3a1f4cb87929ab
Hwan <choihwan2@naver.com>
2020-01-07 21:45:25 +0900

    update:StringBufferTest.java

--- src/selftest/java/interfaces/page84/StringBufferTest.java
+++ src/selftest/java/interfaces/page84/StringBufferTest.java
@@ -5,9 +5,13 @@ public class StringBufferTest {
 	public static void main(String[] args) {
 		StringBuffer sb = new StringBuffer("LGCNS");
 		String str = null;
-		str = "cnser";
-		sb.replace(2, 5, str);
-		System.out.println(sb.toString());
+		str = "null";
+		sb.replace(2, 3, "c");
+		sb.replace(3, 4, "n");
+		sb.replace(4, 5, "s");
+		sb.append("er");
+		str = sb.toString();
+		System.out.println(str.toString());
 	}
 
 }
