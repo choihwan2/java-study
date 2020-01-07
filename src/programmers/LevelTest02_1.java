37ea10a489ef48cb6618d21463c7ac3717217f30
Hwan <choihwan2@naver.com>
2020-01-07 23:00:09 +0900

    add:programers.leveltest02

---
+++ src/programmers/LevelTest02_1.java
@@ -0,0 +1,34 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class LevelTest02_1 {
+
+	public static void main(String[] args) {
+		int[] heights = { 6, 9, 5, 7, 4 };
+		solution(heights);
+		System.out.println(Arrays.toString(solution(heights)));
+	}
+
+	public static int[] solution(int[] heights) {
+		int length = heights.length;
+		int[] answer = new int[length];
+		for (int i = length - 1; i > 0; i--) {
+			if (i < length - 2) {
+				if (heights[i] == heights[i+1] ) {
+					answer[i] = answer[i+1]; 
+					continue;
+				}
+			}
+			
+			for (int j = i - 1; j > 0; j--) {
+				if (heights[i] < heights[j]) {
+					answer[i] = j + 1;
+					break;
+				}
+			}
+		}
+		return answer;
+	}
+	// 6 9 5 7 4
+}
---
+++ src/programmers/LevelTest02_2.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class LevelTest02_2 {
+
+	public static void main(String[] args) {
+		int[] heights = { 1, 2, 3, 2, 3 };
+		System.out.println(Arrays.toString(solution(heights)));
+	}
+
+	public static int[] solution(int[] prices) {
+		int[] answer = new int[prices.length];
+		for (int i = 0; i < answer.length - 1; i++) {
+			for (int j = i + 1; j < answer.length; j++) {
+				if (prices[i] <= prices[j] ) {
+					answer[i]++;
+				}
+			}
+		}
+		return answer;
+	}
+	// 6 9 5 7 4
+}
