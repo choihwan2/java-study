91c7d4adb2031658d88cbf643968980ad249ea15
choihwan2 <choihwan2@naver.com>
2020-01-21 17:33:31 +0900

    Add:stack_ex02.java

---
+++ src/programmers/stack/que/Test02.java
@@ -0,0 +1,34 @@
+package programmers.stack.que;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static int solution(int bridge_length, int weight, int[] truck_weights) {
+		int answer = 0;
+		return answer;
+	}
+
+	public class Train {
+		int time = 0;
+		int weight;
+
+		public Train() {
+			weight = 0;
+		}
+		public Train(int weight) {
+			this.weight = weight;
+		}
+
+		public Train(int time, int weight) {
+			super();
+			this.time = time;
+			this.weight = weight;
+		}
+
+	}
+
+}
