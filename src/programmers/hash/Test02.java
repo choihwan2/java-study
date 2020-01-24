fa0a1b4d40075172377622d0a07b2a08d476f62c
Hwan <choihwan2@naver.com>
2020-01-24 14:27:02 +0900

    Add:hash/test02.java

--- src/programmers/hash/Test02.java
+++ src/programmers/hash/Test02.java
@@ -18,7 +18,7 @@ public class Test02 {
         
         for (int i = 0; i < phone_book.length; i++) {
 			for (int j = 0; j < phone_book.length; j++) {
-				if (hashMap.co) {
+				if (hashMap.) {
 					
 				}
 			}
