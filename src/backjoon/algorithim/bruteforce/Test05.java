76b3f13d3e79f1c22a86530615808cfc4d2facd0
choihwan2 <choihwan2@naver.com>
2020-02-08 16:52:40 +0900

    Solving:BackjoonBruteforceTest05.java

--- src/backjoon/algorithim/bruteforce/Test05.java
+++ src/backjoon/algorithim/bruteforce/Test05.java
@@ -1,33 +1,63 @@
 package backjoon.algorithim.bruteforce;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Test05 {
 	public static void main(String[] args) {
-		ChessBoard chessBoard = new ChessBoard();
-		chessBoard.blackBoard(false);
-		for (int i = 0; i < chessBoard.board.length; i++) {
-			System.out.println(Arrays.toString(chessBoard.board[i]));
+		Scanner scanner = new Scanner(System.in);
+		char[][] testBoard;
+		int x, y;
+		x = scanner.nextInt();
+		y = scanner.nextInt();
+		scanner.nextLine();
+		testBoard = new char[x][y];
+		for (int i = 0; i < testBoard.length; i++) {
+			testBoard[i] = scanner.nextLine().toCharArray();
 		}
+
+		ChessBoard whiteBoard = new ChessBoard();
+		ChessBoard blackBoard = new ChessBoard();
+		whiteBoard.makeBoard(false);
+		blackBoard.makeBoard(true);
+		
+		int white_answer = 0;
+		int black_answer = 0;
+		
+		Math.min(white_answer, black_answer);
+		
+	}
+
+	public static int compareBoard(char[][] board, char[][] chess) {
+		int answer = 0;
+		for (int i = 0; i < chess.length; i++) {
+			for (int j = 0; j < chess.length; j++) {
+				if (board[i][j] != chess[i][j]) {
+					answer++;
+				}
+			}
+		}
+
+		return answer;
 	}
 }
 
-class ChessBoard{
+class ChessBoard {
 	char[][] board;
-	
+
 	public ChessBoard() {
 		this.board = new char[8][8];
 	}
-	
-	public void blackBoard(boolean type) {
+
+	public void makeBoard(boolean type) { // true 면 검은색 false 면 흰색
 		boolean isBlack = type;
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board.length; j++) {
-				if(isBlack) {
+				if (isBlack) {
 					board[i][j] = 'B';
 					isBlack = !isBlack;
-				}else {
-					board[i][j] = 'W'; 
+				} else {
+					board[i][j] = 'W';
 					isBlack = !isBlack;
 				}
 			}
