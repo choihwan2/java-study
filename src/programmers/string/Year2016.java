fcc58bd7d24e00a841d49a056be1c33ed45d4d98
Hwan <choihwan2@naver.com>
2020-02-22 19:01:57 +0900

    Add:ProgrammersYear2016

---
+++ src/programmers/string/Year2016.java
@@ -0,0 +1,21 @@
+package programmers.string;
+
+public class Year2016 {
+
+}
+
+class Solution {
+  public String solution(int a, int b) {
+      String[] days = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+      //0 FRI, 1 SAT, 2 SUN, 3 MON, 4 TUE, 5 WED, 6 THU
+      int[] month = {31,29,31,30,31,30,31,31,30,31,30,31};
+      int diff = 0;
+      for(int i = 1; i<a; i++){
+          diff += month[i-1];
+      }
+      diff += b-1;
+
+      String answer = days[diff%7];
+      return answer;
+  }
+}
