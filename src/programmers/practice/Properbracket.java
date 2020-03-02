7952fc39c87438f1db5ea35cccf79e83bd05d61b
Hwan <choihwan2@naver.com>
2020-03-02 11:26:08 +0900

    Solve:Properbracket

--- src/programmers/practice/Properbracket.java
+++ src/programmers/practice/Properbracket.java
@@ -1,9 +1,11 @@
 package programmers.practice;
 
+import java.util.Stack;
+
 public class Properbracket {
 
 	public static void main(String[] args) {
-		System.out.println(solution("((((())))"));
+		System.out.println(solution2("((((())))))"));
 	}
 	public static boolean solution(String s) {
 		boolean answer = false;
@@ -19,13 +21,40 @@ public class Properbracket {
 		if(a == 0) {
 			answer = true;
 		}
-//		while(s.indexOf("()") >= 0){
-//			s = s.replace("()", "");
-//		}
-//		if(s.length()>=1) {
-//			answer = false;
-//		}
 		
+		return answer;
+	}
+	
+	public static boolean solution2(String s) {
+		boolean answer = false;
+		StringBuffer str = new StringBuffer(s.replace("()", ""));
+		while(str.toString().contains("()")) {
+			str = new StringBuffer(str.toString().replace("()", ""));
+		}
+		if(str.length() == 0) {
+			answer = true;
+		}
+		
+		return answer;
+	}
+	public static boolean solution3(String s) {
+		boolean answer = false;
+		Stack<Character> stack = new Stack<Character>();
+		for(int i =0; i<s.length(); i++) {
+			if(s.charAt(i) =='(') {
+				stack.push(s.charAt(i));
+				continue;
+			}else {
+				if(stack.isEmpty()) {
+					return false;
+				}else {
+					stack.pop();
+				}
+			}
+		}
+		if(stack.isEmpty()) {
+			answer = true;
+		}
 		return answer;
 	}
 
