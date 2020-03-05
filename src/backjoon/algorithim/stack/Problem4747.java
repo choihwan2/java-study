e83702b4490608da08823146978597928fa341e0
Hwan <choihwan2@naver.com>
2020-03-05 14:25:19 +0900

    Solving:Problem4747

---
+++ src/backjoon/algorithim/stack/Problem4747.java
@@ -0,0 +1,69 @@
+package backjoon.algorithim.stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Problem4747 {
+	/*
+	 * 세계는 균형이 잘 잡혀있어야 한다. 양과 음, 빛과 어둠 그리고 왼쪽 괄호와 오른쪽 괄호처럼 말이다. 정민이의 임무는 어떤 문자열이
+	 * 주어졌을 때, 괄호들의 균형이 잘 맞춰져 있는지 판단하는 프로그램을 짜는 것이다.
+	 * 
+	 * 문자열에 포함되는 괄호는 소괄호("()") 와 대괄호("[]")로 2종류이고, 문자열이 균형을 이루는 조건은 아래와 같다.
+	 * 
+	 * 모든 왼쪽 소괄호("(")는 오른쪽 소괄호(")")와만 짝을 이룰 수 있다. 모든 왼쪽 대괄호("[")는 오른쪽 대괄호("]")와만 짝을
+	 * 이룰 수 있다. 모든 오른쪽 괄호들은 자신과 짝을 이룰 수 있는 왼쪽 괄호가 존재한다. 모든 괄호들의 짝은 1:1 매칭만 가능하다. 즉,
+	 * 괄호 하나가 둘 이상의 괄호와 짝지어지지 않는다. 짝을 이루는 두 괄호가 있을 때, 그 사이에 있는 문자열도 균형이 잡혀야 한다. 정민이를
+	 * 도와 문자열이 주어졌을 때 균형잡힌 문자열인지 아닌지를 판단해보자.
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+//		while(scanner.hasNext()) {
+//			String str = scanner.nextLine();
+//			System.out.println(str);
+//			System.out.println("Yes");
+//		}
+		while (scanner.hasNext()) {
+			String str = scanner.nextLine();
+			Stack<Character> stack_s = new Stack<>();
+			Stack<Character> stack_L = new Stack<>();
+			boolean perfect = true;
+			for (int i = 0; i < str.length(); i++) {
+				if (str.charAt(i) == '(') {
+					stack_s.push('(');
+					continue;
+				} else if (str.charAt(i) == ')') {
+					if (!stack_s.isEmpty()) {
+						stack_s.pop();
+					} else {
+						perfect = false;
+						break;
+					}
+				}
+
+				if (str.charAt(i) == '[') {
+					stack_L.push('[');
+					continue;
+				} else if (str.charAt(i) == ']') {
+					if (!stack_L.isEmpty()) {
+						stack_L.pop();
+					} else {
+						perfect = false;
+						break;
+					}
+				}
+			}
+			if (!stack_s.isEmpty()) {
+				perfect = false;
+			}
+			if(!!stack_L.isEmpty()) {
+				perfect = false;
+			}
+			if (perfect) {
+				System.out.println("yes");
+			} else {
+				System.out.println("no");
+			}
+		}
+		scanner.close();
+	}
+}
