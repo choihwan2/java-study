9b3622e836e2f7b71b5b9bfa412027bdeed7caca
Hwan <choihwan2@naver.com>
2020-03-16 20:56:47 +0900

    Add:BackjoonProblem11399'

--- src/programmers/perfectsearch/FindDemicalNumber.java
+++ src/programmers/perfectsearch/FindDemicalNumber.java
@@ -3,6 +3,7 @@ package programmers.perfectsearch;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class FindDemicalNumber {
 
@@ -10,8 +11,15 @@ public class FindDemicalNumber {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		String input = bf.readLine();
 		int[] arr = new int[input.length()];
+		ArrayList<Integer> demicArr = new ArrayList<>();
+		demicArr.add(2);
+		
 		for(int i =0; i<input.length(); i++) {
 			arr[i] = Character.getNumericValue((input.charAt(i))); 
+		}
+		
+		for(int i = 3; i< Math.pow(10,input.length()); i++) {
+			
 		}
 		
 	}
