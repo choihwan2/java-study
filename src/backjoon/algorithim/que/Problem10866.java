d41f7b6df7cea483d9d1d9ed4de9e769112a7da5
Hwan <choihwan2@naver.com>
2020-03-23 22:51:19 +0900

    Update:BackjoonProblem10866

--- src/backjoon/algorithim/que/Problem10866.java
+++ src/backjoon/algorithim/que/Problem10866.java
@@ -3,56 +3,65 @@ package backjoon.algorithim.que;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.LinkedList;
 
 public class Problem10866 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int num = Integer.parseInt(bf.readLine());
+		int case_num = Integer.parseInt(bf.readLine());
 		int front = 0;
-		int back = num - 1;
+		int rear = 0;
+		int num = 0;
 		int[] deque = new int[1000000];
-		Deque<Integer> deque2 = new LinkedList<>();
-		ArrayList<Integer> arrayList = new ArrayList<>();
-		LinkedList<Integer> linkedList = new LinkedList<>();
-		
-		for (int i = 0; i < num; i++) {
+		int max = deque.length;
+
+		for (int i = 0; i < case_num; i++) {
 			String[] input = bf.readLine().split(" ");
 			if (input[0].equals("push_front")) {
-				deque[front++] = Integer.parseInt(input[1]);
+				if(--front < 0)
+					front = max -1;
+				deque[front] = Integer.parseInt(input[1]);
+				num++;
 			} else if (input[0].equals("push_back")) {
-				deque[back--] = Integer.parseInt(input[1]);
+				deque[rear++] = Integer.parseInt(input[1]);
+				num++;
 			} else if (input[0].equals("pop_front")) {
-				if (front != 0) {
-					System.out.println(deque[--front]);
-				} else {
+				if (num<=0) {
 					System.out.println(-1);
+				}else {
+					System.out.println(deque[front++]);
+					if(front == max)
+						front =0;
+					num--;
 				}
 			} else if (input[0].equals("pop_back")) {
-				if (back != num -1) {
-					System.out.println(deque[++back]);
-				} else {
+				if(num <= 0) {
 					System.out.println(-1);
+				}else {
+					if(--rear <0)
+						rear = max - 1;
+					System.out.println(deque[rear]);
+					num--;
 				}
 			} else if (input[0].equals("size")) {
-				int size = front + (num - back -1);
-				System.out.println(size);
+				System.out.println(num);
 			} else if (input[0].equals("front")) {
-				if (front != 0) {
-					System.out.println(deque[front-1]);
-				} else {
+				if (num<=0) {
 					System.out.println(-1);
+				}else {
+					System.out.println(deque[front]);
 				}
 			} else if (input[0].equals("back")) {
-				if (back != num -1) {
-					System.out.println(deque[back+1]);
-				} else {
+				if (num<=0) {
 					System.out.println(-1);
+				}else {
+					if((rear -1) < 0) {
+						System.out.println(deque[max -1]);
+					}else {
+						System.out.println(deque[rear -1]);
+					}
 				}
 			} else if (input[0].equals("empty")) {
-				if (front == 0 && back == (num -1)) {
+				if (num<=0) {
 					System.out.println(1);
 				}else {
 					System.out.println(0);
