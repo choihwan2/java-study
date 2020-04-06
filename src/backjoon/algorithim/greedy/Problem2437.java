5321f6c686c7db3cd78409a03a14635dff5d54ab
Hwan <choihwan2@naver.com>
2020-04-06 21:30:40 +0900

    Solve:Problem2437

---
+++ src/backjoon/algorithim/greedy/Problem2437.java
@@ -0,0 +1,34 @@
+package backjoon.algorithim.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Problem2437 {
+	static boolean[] values;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		int[] arr = new int[num];
+		String[] input = br.readLine().split(" ");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(input[i]);
+		}
+		Arrays.sort(arr);
+		System.out.println(find(arr));
+	}
+
+	public static int find(int[] arr) {
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] <= sum + 1) {
+				sum += arr[i];
+			} else {
+				break;
+			}
+		}
+		return sum + 1;
+	}
+}
