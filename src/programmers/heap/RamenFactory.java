346e52cb16d831f5839a81decc8ae541446499f4
Hwan <choihwan2@naver.com>
2020-04-29 17:47:09 +0900

    Solve:RamenFactory

---
+++ src/programmers/heap/RamenFactory.java
@@ -0,0 +1,57 @@
+package programmers.heap;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+public class RamenFactory {
+
+	public static void main(String[] args) {
+		// 4, [1, 2, 3, 4], [10, 40, 30, 20], 100
+
+		int[] dates = { 1, 2, 3, 4 };
+		int[] supplies = { 10, 40, 30, 20 };
+		int k = 100;
+		System.out.println(solution(4, dates, supplies, k));
+	}
+	/*
+	 * stock 에 있는 값으로 처음에 들어올 dates 값을 정한다.
+	 */
+
+	public static int solution(int stock, int[] dates, int[] supplies, int k) {
+		int answer = 0;
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Comparator.reverseOrder());
+		int index = 0;
+		for (int i = 0; i < k; i++) {
+			if (index < dates.length && i == dates[index]) {
+				pq.add(supplies[index++]);
+			}
+
+			if (stock == 0) {
+				stock += pq.poll();
+				answer++;
+			}
+			stock -= 1;
+		}
+		return answer;
+
+	}
+
+}
+
+class Supply implements Comparable<Supply> {
+	int date;
+	int sup;
+
+	public Supply(int date, int sup) {
+		super();
+		this.date = date;
+		this.sup = sup;
+	}
+
+	@Override
+	public int compareTo(Supply o) {
+		return o.sup > sup ? 1 : -1;
+	}
+
+}
