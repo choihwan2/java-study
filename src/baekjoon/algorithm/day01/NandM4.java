1371906855219af68e4e78696b0e07dc50ab7d7e
Hwan <choihwan2@naver.com>
2020-05-04 14:03:30 +0900

    Solve:NandM4

---
+++ src/baekjoon/algorithm/day01/NandM4.java
@@ -0,0 +1,44 @@
+package baekjoon.algorithm.day01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NandM4 {
+
+	static int[] list;
+	static StringBuilder stb;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] NM = br.readLine().split(" ");
+
+		int N = Integer.parseInt(NM[0]);
+		int M = Integer.parseInt(NM[1]);
+
+		stb = new StringBuilder();
+		list = new int[M];
+
+		sol(N, M, 0);
+		System.out.println(stb);
+
+	}
+
+	public static void sol(int N, int M, int num) {
+		if (num == M) {
+			for (int i : list) {
+				stb.append(i);
+				stb.append(" ");
+			}
+			stb.append("\n");
+			return;
+		}
+
+		for (int i = 1; i <= N; i++) {
+			if (num > 0 && list[num - 1] > i) continue;
+			list[num] = i;
+			sol(N, M, num + 1);
+		}
+	}
+
+}
