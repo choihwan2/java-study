ea99616da3c58569d2396c5d3ff9ae3bb9e18dc2
Hwan <choihwan2@naver.com>
2020-05-28 21:49:16 +0900

    VistedLength add equals method

--- src/programmers/school/day01/VisitedLength2.java
+++ src/programmers/school/day01/VisitedLength2.java
@@ -4,10 +4,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class VisitedLength2 {
-	private final int[] dx = { 0, 0, -1, 1 };
-	private final int[] dy = { 1, -1, 0, 0 };
-	private final int MAX = 5;
-	private final int MIN = -5;
+	private final int[] dx = { 0, 0, -1, 1 }, dy = { 1, -1, 0, 0 };
+	private final int MAX = 5, MIN = -5;
 
 	int x = 0, y = 0;
 
@@ -19,7 +17,7 @@ public class VisitedLength2 {
 
 		for (int i = 0; i < dirs.length(); i++) {
 			char dir = dirs.charAt(i);
-			if (checkGo(changeDir(dir))) {
+			if (checkGo(changeDir(dir)) != null) {
 				tail = head;
 				head = new Position(x, y);
 				Path temPath = new Path(head, tail);
@@ -45,15 +43,15 @@ public class VisitedLength2 {
 		}
 	}
 
-	private boolean checkGo(int dir) {
+	private Position checkGo(int dir) {
 		int tX = x + dx[dir];
 		int tY = y + dy[dir];
 		if (tX >= MIN && tX <= MAX && tY >= MIN && tY <= MAX) {
 			x = tX;
 			y = tY;
-			return true;
+			return new Position(tX, tY);
 		} else {
-			return false;
+			return null;
 		}
 	}
 
@@ -66,16 +64,37 @@ public class VisitedLength2 {
 			this.y = y;
 		}
 
-		
-		
-//		@Override
-//		public boolean equals(Object obj) {
-//			Position compare = (Position) obj;
-//			if (this.x == compare.x && this.y == compare.y) {
-//				return true;
-//			}
-//			return false;
-//		}
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + getEnclosingInstance().hashCode();
+			result = prime * result + x;
+			result = prime * result + y;
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Position other = (Position) obj;
+			if (!getEnclosingInstance().equals(other.getEnclosingInstance()))
+				return false;
+			if (x != other.x)
+				return false;
+			if (y != other.y)
+				return false;
+			return true;
+		}
+
+		private VisitedLength2 getEnclosingInstance() {
+			return VisitedLength2.this;
+		}
 
 	}
 
@@ -87,18 +106,46 @@ public class VisitedLength2 {
 			this.nowP = nowP;
 			this.beforeP = beforeP;
 		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + getEnclosingInstance().hashCode();
+			result = prime * result + ((beforeP == null) ? 0 : beforeP.hashCode());
+			result = prime * result + ((nowP == null) ? 0 : nowP.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Path other = (Path) obj;
+			if (!getEnclosingInstance().equals(other.getEnclosingInstance()))
+				return false;
+			if (beforeP == null) {
+				if (other.beforeP != null)
+					return false;
+			} else if (!beforeP.equals(other.beforeP))
+				return false;
+			if (nowP == null) {
+				if (other.nowP != null)
+					return false;
+			} else if (!nowP.equals(other.nowP))
+				return false;
+			return true;
+		}
+
+		private VisitedLength2 getEnclosingInstance() {
+			return VisitedLength2.this;
+		}
 		
 		
-//		@Override
-//		public boolean equals(Object obj) {
-//			Path compare = (Path) obj;
-//			if (this.nowP.equals(compare.nowP) && this.beforeP.equals(compare.beforeP)) {
-//				return true;
-//			} else if (this.nowP.equals(compare.beforeP) && this.beforeP.equals(compare.nowP)) {
-//				return true;
-//			}
-//			return false;
-//		}
 
 	}
 
