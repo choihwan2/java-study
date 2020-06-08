05c5074cc9b53d5a83b2859b26e1a4f17a19216d
Hwan <choihwan2@naver.com>
2020-06-08 19:25:51 +0900

    Refactoring WordChange recursive -> loop

--- src/programmers/school/day03/WordChange2.java
+++ src/programmers/school/day03/WordChange2.java
@@ -1,26 +1,55 @@
 package programmers.school.day03;
 
-import java.util.Stack;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class WordChange2 {
 
+    private class Word {
+        String word;
+        int cnt;
+
+        public Word(String word, int cnt) {
+            this.word = word;
+            this.cnt = cnt;
+        }
+
+        public String getWord() {
+            return word;
+        }
+
+        public int getCnt() {
+            return cnt;
+        }
+    }
 
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
         boolean[] wordBool = new boolean[words.length];
-        String start = begin;
 
-        for (int i = 0; i < words.length; i++) {
-            Stack<String> stack = new Stack<>();
-            if(isChangeable(begin,words[i])){
-                stack.push(words[i]);
-                start = words[i];
-                wordBool[i] = true;
+        if (!Arrays.asList(words).contains(target))
+            return 0;
+        Queue<Word> que = new LinkedList<>();
+        que.add(new Word(begin, 0));
+
+        while (!que.isEmpty()) {
+            if (que.peek().word.equals(target)) {
+                answer = que.poll().cnt;
+                break;
             }
-            while(!stack.isEmpty() && !stack.peek().equals(target)){
 
+            Word nowWord = que.poll();
+
+            for (int i = 0; i < words.length; i++) {
+                if (wordBool[i]) continue;
+                if (isChangeable(nowWord.word, words[i])) {
+                    wordBool[i] = true;
+                    que.add(new Word(words[i], nowWord.cnt + 1));
+                }
             }
         }
+
         return answer;
     }
 
