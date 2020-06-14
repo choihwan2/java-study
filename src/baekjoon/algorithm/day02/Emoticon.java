766828984e858272fb6571cd979a50e377ad2642
Hwan <choihwan2@naver.com>
2020-06-14 22:34:09 +0900

    Solve: Emoticon

--- src/baekjoon/algorithm/day02/Emoticon.java
+++ src/baekjoon/algorithm/day02/Emoticon.java
@@ -8,18 +8,14 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Emoticon {
-    static final int MAX = 4000;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-        int[][] list = new int[1001][1001];
-
-        for (int i = 0; i < 1000; i++) {
+        int[][] list = new int[N + 1][N +1];
+        for (int i = 0; i < N + 1; i++) {
             Arrays.fill(list[i], -1);
         }
-
         Queue<Integer> que = new LinkedList<>();
         list[1][0] = 0;
 
@@ -30,9 +26,7 @@ public class Emoticon {
             int now = que.poll();
             int copy = que.poll();
 
-            if (now < 0 || now + copy < 0 || now > 1000 || now + copy > 1000 || now -1 <0 || now -1 > 1000) continue;
-
-            if (list[now + copy][copy] == -1) {
+            if (now + copy < N + 1 && list[now + copy][copy] == -1) {
                 list[now + copy][copy] = list[now][copy] + 1;
                 que.add(now + copy);
                 que.add(copy);
@@ -44,20 +38,17 @@ public class Emoticon {
                 que.add(now);
             }
 
-            if (list[now - 1][copy] == -1) {
+            if (now -1 >= 0 && list[now - 1][copy] == -1) {
                 list[now - 1][copy] = list[now][copy] + 1;
                 que.add(now - 1);
                 que.add(copy);
             }
         }
         int answer = -1;
-        for (int i = 0; i < 1000; i++) {
-
+        for (int i = 0; i < N + 1; i++) {
+            if(answer == -1 || answer > list[N][i])
+                answer = list[N][i];
         }
         System.out.println(answer);
-
-
     }
-
-
 }
