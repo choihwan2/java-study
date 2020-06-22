1988d781535aef4551451884ca8044501d4e307a
Hwan <choihwan2@naver.com>
2020-06-22 22:14:33 +0900

    Fail TravelRoute..

--- src/programmers/dfsbfs/TravelRoute.java
+++ src/programmers/dfsbfs/TravelRoute.java
@@ -2,6 +2,7 @@ package programmers.dfsbfs;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 
 public class TravelRoute {
@@ -12,14 +13,20 @@ public class TravelRoute {
         for (String[] ticket : tickets) {
             PriorityQueue<String> que = travelMap.getOrDefault(ticket[0], new PriorityQueue<>());
             que.add(ticket[1]);
-            travelMap.put(ticket[0],que);
+            travelMap.put(ticket[0], que);
         }
+        /*
+        TODO 단순하게 우선순위큐로 해결되는 것이 아니라, 모든 항공권을 사용해야하므로 하나씩 확인을 다해줘야한다. 즉 Bruteforce 방식으로 하나씩 다 검사해주는게 맞는것 같다.
+         */
         String start = "ICN";
         answerList.add(start);
         while (travelMap.containsKey(start)) {
-            String next = travelMap.get(start).poll();
-            answerList.add(next);
-            start = next;
+            if (!travelMap.get(start).isEmpty()) {
+                String next = travelMap.get(start).poll();
+                answerList.add(next);
+                start = next;
+            } else
+                break;
         }
         for (int i = 0; i < answer.length; i++) {
             answer[i] = answerList.get(i);
