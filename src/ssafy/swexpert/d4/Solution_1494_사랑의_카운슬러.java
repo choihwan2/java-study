7ed1aa1d55ac8bd516afb3712c41ac5bf42f8f57
Hwan <choihwan2@naver.com>
2020-09-06 23:27:26 +0900

    Add Solution_1494

---
+++ src/ssafy/swexpert/d4/Solution_1494_사랑의_카운슬러.java
@@ -0,0 +1,15 @@
+package ssafy.swexpert.d4;
+
+public class Solution_1494_사랑의_카운슬러 {
+	static int T;
+	static long min = 0L, tempX = 0, tempY;
+	static boolean[] v;
+	
+	static class P {
+			
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
