e425b6337c2e9408b5be677260b518c279a758ef
Hwan <choihwan2@naver.com>
2020-10-02 23:11:12 +0900

    Solve BOJ_17404_RGB거리_2
    완전탐색이 아닌 DP를 활용해서 풀어봄

--- src/baekjoon/algorithm/dp/BOJ_17404_RGB거리_2.java
+++ src/baekjoon/algorithm/dp/BOJ_17404_RGB거리_2.java
@@ -6,40 +6,47 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ_17404_RGB거리_2 {
-	
-	static final int RED = 0, GREEN = 1, BLUE = 2;
-	static int N,house[][],color[], min = Integer.MAX_VALUE;
-	
+
+	static final int RED = 0, GREEN = 1, BLUE = 2, MAX = 1000001;
+	static int N, house[][], color[][], min = Integer.MAX_VALUE;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer stk = null;
 		N = Integer.parseInt(br.readLine());
 		house = new int[N][3];
+		color = new int[N][3];
 		for (int i = 0; i < N; i++) {
 			stk = new StringTokenizer(br.readLine());
 			for (int j = 0; j < 3; j++) {
-				house[i][j] = Integer.parseInt(stk.nextToken());
+				color[i][j] = Integer.parseInt(stk.nextToken());
 			}
 		}
-		color = new int[N];
-		for (int i = 0; i < 3; i++) {
-			color[0] = i;
-			process(1,house[0][color[0]]);
-		}
-		
-		
-	}
 
-	private static void process(int cnt, int sum) {
-		if(cnt == N) {
-			
-			return;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (i == j)
+					house[0][j] = color[0][j];
+				else
+					house[0][j] = MAX;
+			}
+
+			for (int j = 1; j < N; j++) {
+				house[j][RED] = Math.min(house[j - 1][GREEN], house[j - 1][BLUE]) + color[j][RED];
+				house[j][GREEN] = Math.min(house[j - 1][RED], house[j - 1][BLUE]) + color[j][GREEN];
+				house[j][BLUE] = Math.min(house[j - 1][RED], house[j - 1][GREEN]) + color[j][BLUE];
+			}
+
+			for (int j = 0; j < 3; j++) {
+				if (i == j)
+					continue;
+				min = Math.min(min, house[N - 1][j]);
+			}
+
 		}
 		
-		if(color[cnt -1] == RED) {
-			
-		}
-		
+		System.out.println(min);
+
 	}
 
 }
