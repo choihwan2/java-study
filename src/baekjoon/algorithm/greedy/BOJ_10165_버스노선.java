8ce18efef8b470c7a418995fa73ce8ca0bc890d3
Hwan <choihwan2@naver.com>
2020-10-06 23:57:42 +0900

    Solve BOJ_10165_버스노선
    고민끝에 인터넷 검색을 통해서 문제를 해결하였다.
    
    1. 먼저 선들을 두 가지 그룹으로 나누고 시작점을 기준으로 오름차순 정렬한다. (정상적인 선들), (N-1 ~0 이상으로 가는
    선들)
    
    2. 오름차순으로 정렬한 두그룹들을 순회하면서 가장큰 마지막 정점을 기록하면서 그것보다 작다면 연결리스트에서 제거해주었다.
    
    3. 마지막으로 (N-1 ~ 0 이상) 그룹들이 정상적인 선들을 포함할 수 있는 것들을 제외해주고 index 값이 낮은 순서대로
    정답 배열을 만들어 출력해주었다.

---
+++ src/baekjoon/algorithm/greedy/BOJ_10165_버스노선.java
@@ -0,0 +1,117 @@
+package baekjoon.algorithm.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class BOJ_10165_버스노선 {
+
+	static class Line implements Comparable<Line> {
+		int index;
+		int start;
+		int last;
+
+		public Line(int index, int start, int last) {
+			super();
+			this.index = index;
+			this.start = start;
+			this.last = last;
+		}
+
+		@Override
+		public int compareTo(Line o) {
+			return Integer.compare(this.start, o.start) == 0 ? Integer.compare(o.last, this.last)
+					: Integer.compare(this.start, o.start);
+		}
+
+		@Override
+		public String toString() {
+			return "[index=" + index + ", start=" + start + ", last=" + last + "]";
+		}
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		StringTokenizer stk = null;
+		LinkedList<Line> normalList = new LinkedList<>();
+		LinkedList<Line> reverseList = new LinkedList<>();
+		for (int i = 1; i <= M; i++) {
+			stk = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(stk.nextToken());
+			int last = Integer.parseInt(stk.nextToken());
+
+			if (start < last) {
+				normalList.add(new Line(i, start, last));
+			} else {
+				reverseList.add(new Line(i, start, last));
+			}
+		}
+
+		Collections.sort(normalList);
+		Collections.sort(reverseList);
+
+		int maxLast = -1;
+		Iterator<Line> it = normalList.iterator();
+		while (it.hasNext()) {
+			Line line = it.next();
+			int l = line.last;
+			if (maxLast < l) {
+				maxLast = l;
+			} else {
+				it.remove();
+			}
+		}
+
+		maxLast = -1;
+		int minStart = Integer.MAX_VALUE;
+		if (!reverseList.isEmpty()) {
+			minStart = reverseList.peekFirst().start;
+			Iterator<Line> it2 = reverseList.iterator();
+			while (it2.hasNext()) {
+				Line line = it2.next();
+				int l = line.last;
+				if (maxLast < l) {
+					maxLast = l;
+				} else {
+					it2.remove();
+				}
+			}
+		}
+
+		it = normalList.iterator();
+		while (it.hasNext()) {
+			Line line = it.next();
+			if (line.start >= minStart) {
+				it.remove();
+				continue;
+			} else if (line.last <= maxLast) {
+				it.remove();
+			}
+		}
+
+		LinkedList<Line> answerList = new LinkedList<>();
+		answerList.addAll(normalList);
+		answerList.addAll(reverseList);
+		Collections.sort(answerList, new Comparator<Line>() {
+
+			@Override
+			public int compare(Line o1, Line o2) {
+				return Integer.compare(o1.index, o2.index);
+			}
+		});
+		StringBuilder stb = new StringBuilder();
+		for (Line line : answerList) {
+			stb.append(line.index).append(" ");
+		}
+		System.out.println(stb.toString());
+	}
+
+}
